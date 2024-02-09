2df0d7eb467e699be6d452d7e6255f5a9b0ff6d8
Big Boss <saraviad4444@gmail.com>
2024-02-09 13:50:44 -0700

    main.js update

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Declaring a variable named id and assign it a function
+// This function is a lambda expression that takes the parameter "id"
+// The purpose of this function is to return the element with the id "id"
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+// Declaring a variable named username and assign it a function
+// This function is a lambda expression that takes the parameter "username"
+// The purpose of this function is to return the element with the id "username"
+
+// The first line retrieves the html element with the ID "username"
+// from the document and assigns it to the variable username.
+// The second line returns the value of the username variable.
+// The third line is a lambda expression that takes the parameter "username"
+// The purpose of this function is to return the element with the id "username"
+// The fourth line is a function that takes the parameter "username"
+// The purpose of this function is to return the element with the id "username"
+// The fifth line is a function that takes the parameter "username"
+// The purpose of this function is to return the element with the id "username"
+// The sixth line is a function that takes the parameter "username"
+// The purpose of this function is to return the element with the id "username"
+// The seventh line is a function that takes the parameter "username"
+// The purpose of this function is to return the element with the id "username"
+// The eighth line is a function that takes the parameter "username"
+// The purpose of this function is to return the element with the id "username"
+// The ninth line is a function that takes the parameter "username"
+// The purpose of this function is to return the element with the id "username"
+let username = (username) => document.getElementById(username);
+
+let email = (email) => document.getElementById(email);
+
+let password = (password) => document.getElementById(password);
+
+let form = id("form");
+
+let errorMsg = classes("error");
+
+let successIcon = classes("success-icon");
+
+let failureIcon = classes("failure-icon");
